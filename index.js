5447091d7d632ffea78846980568ee7fa2a3ce20
dalilia <dalila200875yahoo.fr>
2023-08-08 14:55:27 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    
+    return customerName = customerName.toUpperCase();
+    
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+  }
+
+
+  const leastFavoriteCustomer = "";
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'test'
+  }
+//console.log(upperCaseCustomerName());
+/*
+function setBestCustomer(){
+    var bestCustomer="not bob"
+}*/
